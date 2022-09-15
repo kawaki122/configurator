9a47d6e3541ef4f5eb5acb2b82e59d8fffa57832
kawaki122 <kawaki122@gmail.com>
2022-09-15 15:32:56 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,109 @@
+import * as THREE from 'three';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import './style.css'
+
+let scene, camera, renderer, loadedModel, controls;
+let selectedType = "cornet", selectedFlavor = "Blueberry";
+
+initialize();
+animate();
+loadModel();
+lightUp();
+handleCategoryChange();
+handleFlavorChange();
+
+function initialize() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer();
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  scene.background = new THREE.Color("lightgray");
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  camera.position.z = 5;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  controls.update();
+  renderer.render(scene, camera);
+};
+
+
+
+function removeModel() {
+  scene.remove(loadedModel);
+  loadedModel.geometry?.dispose();
+  loadedModel.material?.dispose();
+  loadedModel = undefined; //clear any reference for it to be able to garbage collected
+}
+
+function loadModel() {
+  const loader = new GLTFLoader();
+  loader.load(`./assets/${selectedType}/${selectedFlavor}/Project Name.gltf`, (gltf) => {
+    if (loadedModel) {
+      removeModel();
+    }
+
+    gltf.scene.scale.set(15, 15, 15);
+    gltf.scene.position.y = -1
+    loadedModel = gltf.scene;
+
+    scene.add(gltf.scene);
+  })
+}
+
+function lightUp() {
+  const directionalLight = new THREE.PointLight(0xffffff, 0.3)
+  const directionalLight2 = new THREE.SpotLight(0xffffff, 0.3)
+  const light = new THREE.AmbientLight(0x404040, 3); // soft white light
+
+  directionalLight.position.x = 5;
+  directionalLight.position.y = 10;
+  directionalLight2.position.x = -5;
+  directionalLight2.position.y = 5;
+
+  scene.add(light);
+  scene.add(directionalLight);
+}
+
+function handleCategoryChange() {
+  const element = document.getElementById("selectIceCream");
+  element.value = selectedType;
+  console.log(element.onchange)
+  element.addEventListener("change", (event) => {
+    selectedType = event.target.value;
+    loadModel()
+  });
+}
+
+function handleFlavorChange() {
+  const element = document.getElementById("selectFlavor");
+  element.value = selectedFlavor;
+  element.addEventListener("change", (event) => {
+    selectedFlavor = event.target.value;
+    loadModel()
+  });
+}
+
+// function addPlane() {
+//   const geometry = new THREE.CircleGeometry(50, 20);
+//   const material = new THREE.MeshStandardMaterial({color: "#ffffff"});
+//   const cube = new THREE.Mesh(geometry, material);
+//   cube.rotateX(-(Math.PI/2))
+//   cube.receiveShadow = true;
+//   scene.add(cube);
+// }
+// function addBox() {
+//   const geometry = new THREE.BoxGeometry(2, 2, 2);
+//   const material = new THREE.MeshStandardMaterial({color: "#ffffff"});
+//   const cube = new THREE.Mesh(geometry, material);
+//   cube.castShadow = true;
+//   cube.receiveShadow = true;
+//   cube.position.y = 1;
+//   scene.add(cube);
+// }
